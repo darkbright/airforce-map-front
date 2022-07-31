c38d4370e9717759a26bc359525316c96aec3ce5
Penny-Cho <tinysalmon0@gmail.com>
2022-07-31 13:01:43 +0900

    아이콘 custom svg 맵위에 뿌리는거 구현

--- src/components/map/BaseMap.tsx
+++ src/components/map/BaseMap.tsx
@@ -3,8 +3,6 @@ import D2MapModule from "../../libs/d2/D2MapModule";
 import mapSettings from "../../libs/d2/mapSettings";
 import Loading from "../loading/Loading";
 import MapToolbar from "./MapToolbar";
-import hrCops from "../../data/testCoord/hrCops.json";
-import { sampleIconStyle } from "../../libs/d2/mapSettings/styles/testStyle";
 
 interface BaseMapProps {
 	show?: boolean;
@@ -19,7 +17,8 @@ const BaseMap = ({ show = true }: BaseMapProps) => {
 			{
 				type: "Feature",
 				properties: {
-					name: "남양주",
+					name: "15비",
+					color: "black",
 				},
 				geometry: {
 					type: "Point",
@@ -32,7 +31,8 @@ const BaseMap = ({ show = true }: BaseMapProps) => {
 			{
 				type: "Feature",
 				properties: {
-					name: "이천",
+					name: "19전비",
+					color: "green",
 				},
 				geometry: {
 					type: "Point",
@@ -43,11 +43,56 @@ const BaseMap = ({ show = true }: BaseMapProps) => {
 		],
 	};
 
-	const [loadedData, setLoadedData] = useState(geojsonObject);
 	const [loading, setLoading] = useState(false);
 
+	const svgIcon = `
+	<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"  >
+		<circle  cx="34.4" cy="27" r="19" style="fill:#fff"/>
+		<g>
+			<rect x="8.2" y="23.2"  width="52.4" height="10.4" style="fill:#fff"/>
+			<path d="M62.1,35.1H6.7V21.7h55.4V35.1z M9.7,32.1h49.4v-7.4H9.7V32.1z"/>
+		</g>
+	</svg>
+`;
+
+	const pointStyle = function (feature: any) {
+		const featureId = feature.get("name");
+		const iconColor = feature.get("color");
+
+		const textStyle = new ol.style.Style({
+			text: new ol.style.Text({
+				text: String(featureId),
+				scale: 2,
+				offsetY: 20,
+				fill: new ol.style.Fill({
+					color: [0, 0, 128, 1],
+				}),
+			}),
+		});
+
+		const point = new ol.style.Style({
+			image: new ol.style.Icon({
+				opacity: 1,
+				color: iconColor === "green" ? "#7FFF00" : "#000",
+				src: "data:image/svg+xml;utf8," + escape(svgIcon),
+				scale: 0.5,
+				offset: [-100, -40],
+			}),
+			// image: new ol.style.Circle({
+			// 	radius: 8,
+			// 	fill: new ol.style.Fill({ color: iconColor }),
+			// 	stroke: new ol.style.Stroke({
+			// 		color: [0, 0, 0],
+			// 		width: 1,
+			// 	}),
+			// }),
+		});
+
+		feature.setStyle([point, textStyle]);
+	};
+
 	// console.log("original", geojsonObject.features[0].geometry.coordinates);
-	console.log(new ol.proj.fromLonLat(geojsonObject.features[1].geometry.coordinates));
+	// console.log(new ol.proj.fromLonLat(geojsonObject.features[1].geometry.coordinates));
 
 	const olLayers = new ol.layer.Vector({
 		// source: olSource,
@@ -56,16 +101,7 @@ const BaseMap = ({ show = true }: BaseMapProps) => {
 		}),
 		zIndex: 500, //(디투맵 내부에서는 지도 0 ~ 99, 투명도 300 ~ 499의 인덱스를 사용한다.)
 		// style: sampleIconStyle,
-		style: new ol.style.Style({
-			image: new ol.style.Circle({
-				radius: 7,
-				fill: new ol.style.Fill({ color: "black" }),
-				stroke: new ol.style.Stroke({
-					color: [255, 0, 0],
-					width: 2,
-				}),
-			}),
-		}),
+		style: pointStyle,
 	});
 
 	useEffect(() => {
@@ -89,6 +125,7 @@ const BaseMap = ({ show = true }: BaseMapProps) => {
 			<div style={{ width: "100%" }}>
 				<MapToolbar />
 				<div id="map" className="map" style={{ width: "100%", height: "800px" }} />
+
 				<div id="d2map-coord-bottom" className="d2map-coord-bottom" />
 			</div>
 		</div>
