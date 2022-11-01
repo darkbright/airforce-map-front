b374a1b22f3e5cdd474b2b4086307234a8ee1af3
Penny-Cho <tinysalmon0@gmail.com>
2022-11-01 10:25:37 +0900

    군대부호 선형면형 미리보기 시 물음표 뜨는거 해결

--- src/modules/map/milSymbol/SingleMilitarySymbolBox.tsx
+++ src/modules/map/milSymbol/SingleMilitarySymbolBox.tsx
@@ -6,6 +6,7 @@ import StarOutlineOutlinedIcon from "@mui/icons-material/StarOutlineOutlined";
 import StarOutlinedIcon from "@mui/icons-material/StarOutlined";
 import { MouseEvent, useState } from "react";
 import MilitarySymbolSettingModal from "./MilitarySymbolSettingModal";
+import { getMilSymbolType } from "../../../libs/d2/mapSettings/milSymbols/getMilSymbolType";
 
 interface SingleMilitarySymbolBoxProps {
 	symbol: ModifiedMilSymboListType;
@@ -25,6 +26,18 @@ const SingleMilitarySymbolBox = ({
 	simplified = false,
 }: SingleMilitarySymbolBoxProps) => {
 	const symbolImage = getMilSymbolImage(symbol.cd);
+
+	/**
+	 * 군대부호가 점인지 선 또는 면 등인지에 따라 image를 로드하는 방식이 다름.
+	 * 만약 군대부호가 점이라면 생성된 군대부호의 모양 그대로 렌더링해도 상관없지만,
+	 * 면형이거나 선인 경우 자원을 소모하므로, 미리 따로 만들어진 간략화 버전을 가져와서 보여주는 것이 좋음.
+	 * 따라서 여기서는 해당 군대부호가 면 또는 선인지 확인하는 함수임.
+	 */
+	const checkIsGraphics = getMilSymbolType(symbol.cd);
+
+	// 기존 군대부호에 들어간 *표시를 모두 -로 바꿔 맵서버에 미리보기를 위해 따로 제작되어 있는 전술도식 선형, 면형의 형태를 가져올 수 있게 됨.
+	const sanitizedCd = symbol.cd.replace(/\*/gi, "-");
+
 	const { favoriteSymbols, addToFavoriteSymbols, removeFavoriteSymbol } =
 		useFavoriteMilSymbolStore();
 	const checkCurrentSymbolIsFavorite = favoriteSymbols.find((fav) => fav.cd === symbol.cd);
@@ -83,8 +96,13 @@ const SingleMilitarySymbolBox = ({
 				<SymbolWrapper onClick={onClick}>
 					<img
 						style={{ width: simplified ? 38 : 55, height: simplified ? 38 : 55 }}
-						src={symbolImage?.imgURL}
+						src={
+							checkIsGraphics > 2
+								? `${window.D2MapManager.D2MS_IMAGE}${sanitizedCd}.svg `
+								: symbolImage?.imgURL
+						}
 					/>
+
 					{!simplified && (
 						<Typography variant="subtitle2" sx={{ opacity: 0.6 }}>
 							{symbol.eName}
